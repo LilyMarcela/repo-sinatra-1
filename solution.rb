0a53e7ee702b8b5989eb43d589c6d93167a4952d
Lili <marcela.b.abril@gmail.com>
2018-03-06 20:31:25 -0500

    sinatra first commit

--- solution.rb
+++ solution.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get '/' do
  #which http method is being used, this case GET, / route
-    @nombre = params["nombre"] ? params["nombre"] : "desconocido"
+    @nombre = params["nombre"] ? params["nombre"] : "desconocido!"
     
     erb :index 
 
