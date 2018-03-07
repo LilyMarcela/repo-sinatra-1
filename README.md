53cc32482fd5555eb169f1b872a79116e3112479
Lili <marcela.b.abril@gmail.com>
2018-03-06 20:28:04 -0500

    first sinatra commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# repo-sinatra-1
---
+++ solution.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+get '/' do
+ #which http method is being used, this case GET, / route
+    @nombre = params["nombre"] ? params["nombre"] : "desconocido"
+    
+    erb :index 
+
+end
